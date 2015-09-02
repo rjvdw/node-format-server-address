019bae98f43dd28b83bb6f19ac758387a3731c1e
RJ van der Weide <rj@rdcl.nl>
2015-09-02 22:45:01 +0200

    Changed coding style

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict'
 
-var util = require('util');
+var util = require('util')
 
 
 function formatServerAddress(address) {
-    if (!address || !address.address) {
-        throw new TypeError('Invalid input.');
-    }
+  if (!address || !address.address) {
+    throw new TypeError('Invalid input.')
+  }
 
-    if (address.family === 'IPv6') {
-        return util.format('[%s]:%s', address.address, address.port);
-    }
-    else {
-        return util.format('%s:%s', address.address, address.port);
-    }
+  if (address.family === 'IPv6') {
+    return util.format('[%s]:%s', address.address, address.port)
+  }
+  else {
+    return util.format('%s:%s', address.address, address.port)
+  }
 }
 
-exports = module.exports = formatServerAddress;
+exports = module.exports = formatServerAddress
