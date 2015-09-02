5356fb00348245800d26e905990577fa793d7d73
RJ van der Weide <rj@rdcl.nl>
2015-09-02 22:46:11 +0200

    Changed coding style

--- test/test.js
+++ test/test.js
@@ -1,32 +1,32 @@
-"use strict";
+'use strict'
 
-var expect = require('chai').expect;
-var formatServerAddress = require('..');
+var expect = require('chai').expect
+var formatServerAddress = require('..')
 
 describe('#formatServerAddress', function () {
-    it('should throw a TypeError if an invalid argument is passed', function () {
-        expect(function () {
-            formatServerAddress(null);
-        }).to.throw(TypeError);
-    });
+  it('should throw a TypeError if an invalid argument is passed', function () {
+    expect(function () {
+      formatServerAddress(null)
+    }).to.throw(TypeError)
+  })
 
-    it('should correctly format IPV4 addresses', function () {
-        var result = formatServerAddress({
-            address: '127.0.0.1',
-            family: 'IPv4',
-            port: 80,
-        });
+  it('should correctly format IPV4 addresses', function () {
+    var result = formatServerAddress({
+      address: '127.0.0.1',
+      family: 'IPv4',
+      port: 80,
+    })
 
-        expect(result).to.equal('127.0.0.1:80');
-    });
+    expect(result).to.equal('127.0.0.1:80')
+  })
 
-    it('should correctly format IPV6 addresses', function () {
-        var result = formatServerAddress({
-            address: '::',
-            family: 'IPv6',
-            port: 443,
-        });
+  it('should correctly format IPV6 addresses', function () {
+    var result = formatServerAddress({
+      address: '::',
+      family: 'IPv6',
+      port: 443,
+    })
 
-        expect(result).to.equal('[::]:443');
-    });
-});
+    expect(result).to.equal('[::]:443')
+  })
+})
